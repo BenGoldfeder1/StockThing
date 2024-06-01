2ccfb778c3f00d20db0222d29ee351e1d8e1cf81
Ben <bennyeg17@gmail.com>
2024-06-01 21:53:54 +0000

    v1 Done

--- Beta Value Detection/highbeta.py
+++ Beta Value Detection/highbeta.py
@@ -10,14 +10,22 @@ nasdaq_stocks = stonks[0].tolist()
 for i in range(len(nasdaq_stocks)):
     nasdaq_stocks[i] = yf.Ticker(nasdaq_stocks[i])
 
+# Loop through the entire list and get rid of the ones that don't have a ticker
+for stock in nasdaq_stocks:
+    if not hasattr(stock.info, 'symbol'):
+        nasdaq_stocks.remove(stock)
+
 # Create a list to store high beta stocks
 high_beta_stocks = []
 
-for stock in nasdaq_stocks:
-    if not stock.info.get('beta') or stock.info['beta'] < 1 or stock.info.get('marketCap') < 50000000:
-        high_beta_stocks.append(stock.ticker)
+with open('/workspaces/StockThing/Beta Value Detection/high_beta_stocks.txt', 'w') as file:
+    for stock in nasdaq_stocks:
+        if ('beta' in stock.info) and ('marketCap' in stock.info):
+            if (stock.info['beta'] > 1) and (stock.info['marketCap'] > 50000000):
+                high_beta_stocks.append(stock.ticker)
+                file.write(stock.info['symbol'] + '\n')
 
 # Write the high beta stocks to a text file
-with open('/workspaces/StockThing/Beta Value Detection/high_beta_stocks.txt', 'w') as file:
-    for stock in high_beta_stocks:
-        file.write(stock + '\n')
+# with open('/workspaces/StockThing/Beta Value Detection/high_beta_stocks.txt', 'w') as file:
+#    for stock in high_beta_stocks:
+#        file.write(stock + '\n')
